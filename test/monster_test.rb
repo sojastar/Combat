d7c34340021dd393125c0e9c3e04c8036e1a7015
Julien Jassaud <sojastar07@gmail.com>
2023-01-17 14:17:15 +0100

    Add Combat::Monster#attack and tests for the whole class

--- test/monster_test.rb
+++ test/monster_test.rb
@@ -3,5 +3,36 @@ require_relative 'test_helper.rb'
 
 describe Combat::Monster do
   it 'creates monsters' do
+    m = Combat::Monster::new_skeleton
+
+    assert_equal    :skeleton,                              m.type
+
+    skeleton_template = Combat::Monster::MONSTERS[m.type]
+
+    assert_includes skeleton_template[:health_point_range], m.health_points
+    assert_equal    skeleton_template[:name],               m.name
+    assert_equal    skeleton_template[:armor],              m.armor
+    assert_equal    skeleton_template[:magic_armor],        m.magic_armor
+  end
+
+  it 'attacks' do
+    m   = Combat::Monster::new_warlock
+    a1  = m.attack 0.3
+    a2  = m.attack 0.6
+    a3  = m.attack 0.9
+
+    attacks = Combat::Monster::MONSTERS[:warlock][:attacks]
+
+    assert_equal    "wand strike",            a1[:name]
+    assert_equal    :physical,                a1[:type]
+    assert_includes attacks[0][:hits_range],  a1[:hits]
+
+    assert_equal    "fire",                   a2[:name]
+    assert_equal    :magical,                 a2[:type]
+    assert_includes attacks[1][:hits_range],  a2[:hits]
+
+    assert_equal    "thunder",                a3[:name]
+    assert_equal    :magical,                 a3[:type]
+    assert_includes attacks[2][:hits_range],  a3[:hits]
   end
 end
