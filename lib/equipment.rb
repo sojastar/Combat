9a0522dc5eca0bc98dd45a04fe78fd57262b7773
Julien Jassaud <sojastar07@gmail.com>
2023-03-03 23:48:40 +0100

    Moved spells and equipment constants out of the module definition files to dedicated files

--- lib/equipment.rb
+++ lib/equipment.rb
@@ -1,27 +1,5 @@
 module Combat
   module Equipment
-    PIECES  = { long_sword:       { name:       "Long Sword",
-                                    body_part:  :left_hand,
-                                    effects:    [ { type: :buff, on: :attack, value: 2 } ] },
-                poisoned_dagger:  { name:       "Poisoned Dagger",
-                                    body_part:  :left_hand,
-                                    effects:    [ { type: :buff, on: :attack, value: 1 },
-                                                  { type: :ailment, name: 'poison', on: :health, value: 1..3, turns: 3 } ] },
-                magic_sword:      { name:       "Magic Sword",
-                                    body_part:  :left_hand,
-                                    effects:    [ { type: :buff, on: :attack, value: 1 },
-                                                  { type: :buff, on: :magic_attack, value: 1 } ] },
-                leather_armor:    { name:       'Leather Armor',
-                                    body_part:  :torso,
-                                    effects:    [ { type: :buff, on: :defense, value: 2 } ] },
-                amulet:           { name:       'Amuleleft_handt',
-                                    body_part:  :neck,
-                                    effects:    [ { type: :buff, on: :magic_defense, value: 2 } ] },
-                magic_helm:       { name:       'Magic Helm',
-                                    body_part:  :head,
-                                    effects:    [ { type: :buff, on: :defense, value: 1 },
-                                                  { type: :buff, on: :magic_defense, value: 1 } ] } }
-
     def self.name(piece)
       PIECES[piece][:name]
     end
